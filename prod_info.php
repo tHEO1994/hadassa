9aef18a1bb845578853c17e885ef9a148b0b58c7
tHEO1994 <tpee935@gmail.com>
2017-04-28 11:45:48 +0000

    product view

--- prod_info.php
+++ prod_info.php
@@ -20,10 +20,10 @@ require_once('includes/menu.inc');
       <h3><b>Product Name</b></h3>
       <h4>Size: 32</h4>
       <h4>Category: Men</h4>
+      <br />
+
       <h4>Description</h4>
-      <br>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
